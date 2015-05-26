e8014a79981b0b4d8195d07c1b028eb38114afd4
Shekhr Ullah <shekharabdullah88@gmail.com>
2015-05-27 00:32:12 +0600

    Project Skeliton

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -10,5 +10,7 @@ public class Application extends Controller {
     public static Result index() {
         return ok(index.render("Your new application is ready."));
     }
-
+    public static Result registration() {
+        return ok(registration.render("Your new application is ready."));
+    }
 }
