dd0a2b9d1f4896c527412d895b76c5fb2476df82
Shekhr Ullah <shekharullah@gmail.com>
2015-05-27 22:43:20 +0600

    Added Code Convention to the Controller->Application.java

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,3 +1,17 @@
+/*
+ * @(#)Application.java        1.0 2015/05/25
+ *
+ * Copyright (c) 2015-2016 Online Doctors Planet.
+ * 11 North Bishil Road, Mirpur 1, Dhaka, Bangladesh
+ * All rights reserved.
+ *
+ * This software is the confidential and proprietary information of Online
+ * Doctors Planet.("Confidential Information").  You shall not
+ * disclose such Confidential Information and shall use it only in
+ * accordance with the terms of the license agreement you entered into
+ * with Online Doctors Planet.
+ */
+
 package controllers;
 
 import play.*;
@@ -5,21 +19,28 @@ import play.mvc.*;
 
 import views.html.*;
 
+/**
+ *  
+         Application.java is written for page rendering and navigation purpose
+ *
+ * @version      
+         1.0 25th May 2015  * @author          
+         Shekhar Ullah  */
 public class Application extends Controller {
 
     public static Result index() {
-        return ok(index.render("Your new application is ready."));
+        return ok(index.render("landing page."));
     }
     
     public static Result registration() {
-        return ok(registration.render("Your new application is ready."));
+        return ok(registration.render("registration."));
     }
     
     public static Result login() {
-        return ok(login.render("Your new application is ready."));
+        return ok(login.render("login."));
     }
     
     public static Result forgotpassword() {
-        return ok(forgotpassword.render("Your new application is ready."));
+        return ok(forgotpassword.render("forgot password."));
     }
 }
